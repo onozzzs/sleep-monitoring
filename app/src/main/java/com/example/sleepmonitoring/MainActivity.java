79a5121d81733bbf21b38c22b484daf92ddf0997
onozzzs <dnwlals00@gmail.com>
2023-11-14 17:54:40 +0900

    first commit

---
+++ app/src/main/java/com/example/sleepmonitoring/MainActivity.java
@@ -0,0 +1,213 @@
+package com.example.sleepmonitoring;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+    private static final int REQUEST_ENABLE_BT = 10;
+    private BluetoothAdapter bluetoothAdapter;
+    private Set<BluetoothDevice> devices;
+    private BluetoothDevice bluetoothDevice;
+    private BluetoothSocket bluetoothSocket = null;
+    private OutputStream outputStream = null;
+    private InputStream inputStream = null;
+    private Thread workerThread = null;
+    private byte[] readBuffer;
+    private int readBufferPosition;
+    int pariedDeviceCount;
+    boolean connect_status;
+    private static final UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setBluetooth();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void setBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 블루투스 어댑터를 디폴트 어댑터로 설정
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "블루투스 미지원 기기입니다.", Toast.LENGTH_LONG).show();
+        } else {
+            if (bluetoothAdapter.isEnabled()) { // 블루투스가 활성화 상태 (기기에 블루투스가 켜져있음)
+                selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+            } else { // 블루투스가 비 활성화 상태 (기기에 블루투스가 꺼져있음)
+                // 블루투스를 활성화 하기 위한 다이얼로그 출력
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // 선택한 값이 onActivityResult 함수에서 콜백된다.
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void selectBluetoothDevice() {
+        devices = bluetoothAdapter.getBondedDevices();
+        // 페어링 된 디바이스의 크기를 저장
+        pariedDeviceCount = devices.size();
+        // 페어링 되어있는 장치가 없는 경우
+        if (pariedDeviceCount == 0) {
+            // 페어링을 하기위한 함수 호출
+            Toast.makeText(getApplicationContext(), "먼저 Bluetooth 설정에 들어가 페어링 해주세요", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // 디바이스를 선택하기 위한 다이얼로그 생성
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("페어링 되어있는 블루투스 디바이스 목록");
+            // 페어링 된 각각의 디바이스의 이름과 주소를 저장
+            List<String> list = new ArrayList<>();
+            // 모든 디바이스의 이름을 리스트에 추가
+            for (BluetoothDevice bluetoothDevice : devices) {
+                list.add(bluetoothDevice.getName());
+            }
+
+            list.add("취소");
+            // List를 CharSequence 배열로 변경
+            final CharSequence[] charSequences = list.toArray(new CharSequence[list.size()]);
+            list.toArray(new CharSequence[list.size()]);
+
+            // 해당 아이템을 눌렀을 때 호출 되는 이벤트 리스너
+            builder.setItems(charSequences, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // 해당 디바이스와 연결하는 함수 호출
+                    connectDevice(charSequences[which].toString());
+                }
+            });
+            // 뒤로가기 버튼 누를 때 창이 안닫히도록 설정
+            builder.setCancelable(false);
+            // 다이얼로그 생성
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    public void connectDevice(String deviceName) {
+        // 페어링 된 디바이스들을 모두 탐색
+        for (BluetoothDevice tempDevice : devices) {
+            // 사용자가 선택한 이름과 같은 디바이스로 설정하고 반복문 종료
+            if (deviceName.equals(tempDevice.getName())) {
+                bluetoothDevice = tempDevice;
+                break;
+            }
+        }
+        Toast.makeText(this, bluetoothDevice + "연결 완료", Toast.LENGTH_SHORT).show();
+        connect_status = true;
+        // Rfcomm 채널을 통해 블루투스 디바이스와 통신하는 소켓 생성
+
+        try {
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            bluetoothSocket.connect();
+        } catch (IOException ex) {
+            Log.e("", ex.getMessage());
+            try {
+                bluetoothSocket = (BluetoothSocket) bluetoothDevice.getClass().getMethod("createRfcommSocket", new Class[]{int.class}).invoke(bluetoothDevice, 1);
+                bluetoothSocket.connect();
+            } catch (Exception e2) {
+                Log.e("error", "could not establish");
+            }
+        }
+        try{
+            Log.v("verbose", "connect device2");
+            outputStream = bluetoothSocket.getOutputStream();
+            inputStream = bluetoothSocket.getInputStream();
+
+            Log.v("verbose", "receive data");
+            // 데이터 수신 함수 호출
+            receiveData();
+
+        } catch (IOException e) {
+            Log.e("error", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    // receiveData 메서드에서 text 값을 활용
+    public void receiveData() {
+        final Handler handler = new Handler();
+        //데이터 수신을 위한 버퍼 생성
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+
+        //데이터 수신을 위한 쓰레드 생성
+        workerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        //데이터 수신 확인
+                        int byteAvailable = inputStream.available();
+                        //데이터 수신 된 경우
+                        if (byteAvailable > 0) {
+                            Log.d(TAG, "run: text = 데이터 수신");
+                            //입력 스트림에서 바이트 단위로 읽어옴
+                            byte[] bytes = new byte[byteAvailable];
+                            inputStream.read(bytes);
+                            //입력 스트림 바이트를 한 바이트씩 읽어옴
+                            for (int i = 0; i < byteAvailable; i++) {
+                                byte tempByte = bytes[i];
+                                //개행문자를 기준으로 받음 (한줄)
+                                if (tempByte == '\n') {
+                                    //readBuffer 배열을 encodeBytes로 복사
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    //인코딩 된 바이트 배열을 문자열로 변환
+                                    final String text = new String(encodedBytes, "UTF-8");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Log.d(TAG, "run: text =" + text);
+                                        }
+                                    });
+                                } else {
+                                    readBuffer[readBufferPosition++] = tempByte;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        workerThread.start();
+    }
+}
